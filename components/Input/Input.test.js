2c0b7a969f2c026db4efda4c4df1c7de0b2dc9a5
Donatella Crisante <dona.crisante@zoho.com>
2023-06-19 10:39:10 +0200

    test navigation and input

--- components/Input/Input.test.js
+++ components/Input/Input.test.js
@@ -2,6 +2,33 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+    render(<Input placeholder="e.g Dodelido" name="nameOfGame" labelText="Name of game" />);
+    const inputName = screen.getByLabelText("Name of game");
+    
+    expect(inputName).toHaveAttribute("name", "NameOfGame");
+    expect(inputName).toHaveAttribute()
 
-test("calls callback on every user input", async () => {});
+});
+
+test("calls callback on every user input", async () => {
+    const user = userEvent.setup();
+    const userInput = jest.fn();
+    render(<Input labelText="Name" name="name" onChange={userInput} />);
+
+    const nameInput = screen.getByLabelText("Name");
+    await user.type(nameInput, "Hello");
+
+    expect(userInput).toHaveBeenCalledTimes(5);
+});
+
+
+
+
+/* You can now go a step further and not only check for a component being rendered, but also for its callback function being called.
+
+Switch to [`Input.test.js`](components/Input/Input.test.js) and write the tests for all given acceptance criteria.
+
+- To check for attributes like `placeholder` or `name`, you can use the matcher `toHaveAttribute(attributeName, value)`.
+- The `Input` component receives an `onChange` callback; in order to test it, remember that you have to mock if first.
+- You can check how often a function has been called with `toHaveBeenCalledTimes(numberOfExpectedCalls)`. */
--- components/Navigation/Navigation.test.js
+++ components/Navigation/Navigation.test.js
@@ -7,4 +7,17 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders with two links 'Play' and 'History'", () => {});
+test("renders with two links 'Play' and 'History'", () => {
+  render(<Navigation />);
+  const playLink = screen.getByRole("link", { name: "Play" });
+  const historyLink = screen.getByRole("link", { name: "History" });
+expect(playLink).toBeInTheDocument();
+expect(historyLink).toBeInTheDocument();
+});
+
+
+/* Switch to [`Navigation.test.js`](components/Navigation/Navigation.test.js) and check whether the navigation is rendered.
+
+- In the test file, `next/router` is already mocked, so you don't need to care about the routing.
+- The exact acceptance criteria is also already defined inside of the test case. You just have to implement the test code inside the return statement of the `test()` function.
+- An HTML `<a>` tag has the accessible role "link". */
